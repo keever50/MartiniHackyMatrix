bd137d2dbf78b5b72e350785e4eb14a62adc4872
MartiniMarter <kevinwit1999@gmail.com>
2023-10-22 00:38:12 +0200

    Tool to set time via USB

---
+++ setclock.py
@@ -0,0 +1,16 @@
+import serial
+import datetime
+import math
+
+print("Enter port:")
+portstr = input()
+ser = serial.Serial(portstr, 115200)
+
+time = math.floor(datetime.datetime.now(datetime.timezone.utc).timestamp()+3600)
+print(time)
+ser.write(b"TIME")
+ser.write(str(time).encode())
+ser.write(b'\r')
+ser.close()
+print("Done")
+input()
